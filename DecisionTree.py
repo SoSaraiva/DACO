ca402de69797d2322f0dd1dbf1d53969ec97e82c
SOFIA SARAIVA <an.saraiva08@gmail.com>
2023-12-29 16:50:28 +0000

    more imports

--- DecisionTree.py
+++ DecisionTree.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from sklearn import tree
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
@@ -11,5 +12,8 @@ from sklearn.metrics import accuracy_score
 
 # Load dataset
 dataset= pd.read_csv('dataset.csv')
+
+dataset.head()
+
 
 
