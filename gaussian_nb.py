20637f4d60573238fc1b5ebb7df42afd0d60c5bc
Joana Fernandes <joanasfernandes02@gmail.com>
2024-01-02 15:34:48 +0000

    Added training on rows with 'Unknown' values

--- gaussian_nb.py
+++ gaussian_nb.py
@@ -1,10 +1,8 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import make_scorer
 from sklearn.model_selection import cross_val_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import StratifiedKFold
 
 
 
@@ -26,6 +24,17 @@ categorical_columns = [
     'Birth defects', 'Blood test result', 'Symptom 1', 'Symptom 2', 'Symptom 3', 'Symptom 4', 'Symptom 5'
 ]
 
+all_x_columns= [
+    'Patient Age', "Genes in mother's side", 'Inherited from father', 'Maternal gene', 'Paternal gene',
+    'Blood cell count (mcL)', "Mother's age", "Father's age", 'Status', 'Respiratory Rate (breaths/min)',
+    'Heart Rate (rates/min)', 'Parental consent', 'Follow-up', 'Gender', 'Birth asphyxia',
+    'Autopsy shows birth defect (if applicable)', 'Place of birth', 'Folic acid details (peri-conceptional)',
+    'H/O serious maternal illness', 'H/O radiation exposure (x-ray)', 'H/O substance abuse',
+    'Assisted conception IVF/ART', 'History of anomalies in previous pregnancies', 'No. of previous abortion',
+    'Birth defects', 'White Blood cell count (thousand per microliter)', 'Blood test result',
+    'Symptom 1', 'Symptom 2', 'Symptom 3', 'Symptom 4', 'Symptom 5',
+]
+
 # Model used
 nb = GaussianNB()
 
@@ -38,6 +47,54 @@ print('-DATASET COUNTS BEFORE DROPPING UNKNOWNS-\n')
 
 print(df.iloc[:, -2].value_counts())
 print(df.iloc[:, -1].value_counts())
+
+
+# Train models without unknown targets
+
+print('\n\n-------------------------REMOVING UNKNOWNS IN TARGET COLUMNS---------------------------\n')
+
+# Replace 'Unknown' with NaN
+df['Genetic Disorder'].replace('Unknown', np.nan, inplace=True)
+df['Disorder Subclass'].replace('Unknown', np.nan, inplace=True)
+
+df.dropna(inplace=True)
+
+print('-DATASET COUNTS AFTER DROPPING TARGET UNKNOWNS-\n')
+print(df.iloc[:, -2].value_counts())
+print(df.iloc[:, -1].value_counts())
+
+# Prepare catgorical data
+
+df_encoded=df.copy()
+for column in all_x_columns:
+    df_encoded[column] = label_encoder.fit_transform(df_encoded[column].astype(str))
+
+
+# Train model for the second-to-last column
+
+# Extract features and target
+X = df_encoded.iloc[:, :-2]
+y = df_encoded.iloc[:, -2]
+
+# Training and Testing of the model
+scores = cross_val_score(nb, X, y, cv=10, scoring='f1_macro')  # evaluate the model scores
+
+meanF1 = np.mean(scores)
+
+print('\n-MODEL PREFORMANCE-\n')
+
+print("F1 Score Macro-Averaged\n Second-to-Last Column=", meanF1)
+
+
+# Train model for the last column
+y = df_encoded.iloc[:, -1]
+
+# Training and Testing of the model
+scores = cross_val_score(nb, X, y, cv=10, scoring='f1_macro')  # evaluate the model scores
+
+meanF1 = np.mean(scores)
+
+print("\n Last Column=", meanF1)
 
 
 # Train models without unknown data
@@ -55,15 +112,16 @@ print(df.iloc[:, -1].value_counts())
 
 # Prepare catgorical data
 
+df_encoded=df.copy()
 for column in categorical_columns:
-    df[column] = label_encoder.fit_transform(df[column].astype(str))
+    df_encoded[column] = label_encoder.fit_transform(df_encoded[column].astype(str))
 
 
 # Train model for the second-to-last column
 
 # Extract features and target
-X = df.iloc[:, :-2]
-y = df.iloc[:, -2]
+X = df_encoded.iloc[:, :-2]
+y = df_encoded.iloc[:, -2]
 
 # Training and Testing of the model
 scores = cross_val_score(nb, X, y, cv=10, scoring='f1_macro')  # evaluate the model scores
@@ -76,7 +134,7 @@ print("F1 Score Macro-Averaged\n Second-to-Last Column=", meanF1)
 
 
 # Train model for the last column
-y = df.iloc[:, -1]
+y = df_encoded.iloc[:, -1]
 
 # Training and Testing of the model
 scores = cross_val_score(nb, X, y, cv=10, scoring='f1_macro')  # evaluate the model scores
