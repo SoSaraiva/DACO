e10498ca367764fb56d84cc45a68843a086bc1be
Joana Fernandes <joanasfernandes02@gmail.com>
2024-01-07 20:17:34 +0000

    One-hot encoding for unordered data

--- multinomial_nb.py
+++ multinomial_nb.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.preprocessing import LabelEncoder
 
@@ -12,42 +13,16 @@ df = pd.read_csv('dataset.csv')
 # Encode categorical variables
 label_encoder = LabelEncoder()
 
-
-# List of categorical columns to encode
-categorical_columns = [
-    'Genes in mother\'s side', 'Inherited from father', 'Maternal gene', 'Paternal gene',
-    'Parental consent', 'Follow-up', 'Gender', 'Birth asphyxia',
-    'Autopsy shows birth defect (if applicable)', 'Place of birth',
-    'Folic acid details (peri-conceptional)', 'H/O serious maternal illness',
-    'H/O radiation exposure (x-ray)', 'H/O substance abuse', 'Status', 'Respiratory Rate (breaths/min)', 'Heart Rate (rates/min)',
-    'Assisted conception IVF/ART', 'History of anomalies in previous pregnancies',
-    'Birth defects', 'Blood test result', 'Symptom 1', 'Symptom 2', 'Symptom 3', 'Symptom 4', 'Symptom 5'
-]
-
 all_x_columns= [
     'Patient Age', "Genes in mother's side", 'Inherited from father', 'Maternal gene', 'Paternal gene',
     'Blood cell count (mcL)', "Mother's age", "Father's age", 'Status', 'Respiratory Rate (breaths/min)',
-    'Heart Rate (rates/min)', 'Parental consent', 'Follow-up', 'Gender', 'Birth asphyxia',
+    'Heart Rate (rates/min)', 'Follow-up', 'Gender', 'Birth asphyxia',
     'Autopsy shows birth defect (if applicable)', 'Place of birth', 'Folic acid details (peri-conceptional)',
     'H/O serious maternal illness', 'H/O radiation exposure (x-ray)', 'H/O substance abuse',
     'Assisted conception IVF/ART', 'History of anomalies in previous pregnancies', 'No. of previous abortion',
     'Birth defects', 'White Blood cell count (thousand per microliter)', 'Blood test result',
-    'Symptom 1', 'Symptom 2', 'Symptom 3', 'Symptom 4', 'Symptom 5',
+    'Symptom 1', 'Symptom 2', 'Symptom 3', 'Symptom 4', 'Symptom 5'
 ]
-
-# Model used
-nb = MultinomialNB()
-
-
-
-
-# Print df stats
-
-print('-DATASET COUNTS BEFORE DROPPING UNKNOWNS-\n')
-
-print(df.iloc[:, -2].value_counts())
-print(df.iloc[:, -1].value_counts())
-
 
 # Train models without unknown targets
 
@@ -58,10 +33,6 @@ df['Genetic Disorder'].replace('Unknown', np.nan, inplace=True)
 df['Disorder Subclass'].replace('Unknown', np.nan, inplace=True)
 
 df.dropna(inplace=True)
-
-print('-DATASET COUNTS AFTER DROPPING TARGET UNKNOWNS-\n')
-print(df.iloc[:, -2].value_counts())
-print(df.iloc[:, -1].value_counts())
 
 # Prepare catgorical data
 
@@ -76,25 +47,37 @@ for column in all_x_columns:
 X = df_encoded.iloc[:, :-2]
 y = df_encoded.iloc[:, -2]
 
-# Training and Testing of the model
-scores = cross_val_score(nb, X, y, cv=10, scoring='f1_macro')  # evaluate the model scores
+# Split the data into validation, validation and test
 
-meanF1 = np.mean(scores)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75,  random_state=1) # training as 75% 
+
+# Model used
+nb = MultinomialNB()
+nb.fit(X_train, y_train)
+
+y_pred = nb.predict(X_test) 
 
 print('\n-MODEL PREFORMANCE-\n')
 
-print("F1 Score Macro-Averaged\n Second-to-Last Column=", meanF1)
+print("Second-to-Last Column:\n")
+
+print(classification_report(y_test, y_pred))
 
 
 # Train model for the last column
 y = df_encoded.iloc[:, -1]
 
-# Training and Testing of the model
-scores = cross_val_score(nb, X, y, cv=10, scoring='f1_macro')  # evaluate the model scores
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75,  random_state=1) # training as 75% 
 
-meanF1 = np.mean(scores)
+# Model used
+nb = MultinomialNB()
+nb.fit(X_train, y_train)
 
-print("\n Last Column=", meanF1)
+y_pred = nb.predict(X_test) 
+
+print("\nLast Column:\n")
+
+print(classification_report(y_test, y_pred))
 
 
 # Train models without unknown data
@@ -106,14 +89,10 @@ df.replace('Unknown', np.nan, inplace=True)
 
 df.dropna(inplace=True)
 
-print('-DATASET COUNTS AFTER DROPPING ALL UNKNOWNS-\n')
-print(df.iloc[:, -2].value_counts())
-print(df.iloc[:, -1].value_counts())
-
 # Prepare catgorical data
 
 df_encoded=df.copy()
-for column in categorical_columns:
+for column in all_x_columns:
     df_encoded[column] = label_encoder.fit_transform(df_encoded[column].astype(str))
 
 
@@ -123,22 +102,34 @@ for column in categorical_columns:
 X = df_encoded.iloc[:, :-2]
 y = df_encoded.iloc[:, -2]
 
-# Training and Testing of the model
-scores = cross_val_score(nb, X, y, cv=10, scoring='f1_macro')  # evaluate the model scores
+# Split the data into validation, validation and test
 
-meanF1 = np.mean(scores)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75,  random_state=1) # training as 75% 
+
+# Model used
+nb = MultinomialNB()
+nb.fit(X_train, y_train)
+
+y_pred = nb.predict(X_test) 
 
 print('\n-MODEL PREFORMANCE-\n')
 
-print("F1 Score Macro-Averaged\n Second-to-Last Column=", meanF1)
+print("Second-to-Last Column:\n")
+
+print(classification_report(y_test, y_pred))
 
 
 # Train model for the last column
 y = df_encoded.iloc[:, -1]
 
-# Training and Testing of the model
-scores = cross_val_score(nb, X, y, cv=10, scoring='f1_macro')  # evaluate the model scores
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75,  random_state=1) # training as 75% 
 
-meanF1 = np.mean(scores)
+# Model used
+nb = MultinomialNB()
+nb.fit(X_train, y_train)
 
-print("\n Last Column=", meanF1)
+y_pred = nb.predict(X_test) 
+
+print("\nLast Column:\n")
+
+print(classification_report(y_test, y_pred))
--- multinomial_nb_onehot.py
+++ multinomial_nb_onehot.py
@@ -2,10 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.preprocessing import LabelEncoder
-
-
 
 
 df = pd.read_csv('dataset.csv')
@@ -13,16 +11,21 @@ df = pd.read_csv('dataset.csv')
 # Encode categorical variables
 label_encoder = LabelEncoder()
 
-all_x_columns= [
-    'Patient Age', "Genes in mother's side", 'Inherited from father', 'Maternal gene', 'Paternal gene',
-    'Blood cell count (mcL)', "Mother's age", "Father's age", 'Status', 'Respiratory Rate (breaths/min)',
-    'Heart Rate (rates/min)', 'Follow-up', 'Gender', 'Birth asphyxia',
-    'Autopsy shows birth defect (if applicable)', 'Place of birth', 'Folic acid details (peri-conceptional)',
-    'H/O serious maternal illness', 'H/O radiation exposure (x-ray)', 'H/O substance abuse',
-    'Assisted conception IVF/ART', 'History of anomalies in previous pregnancies', 'No. of previous abortion',
-    'Birth defects', 'White Blood cell count (thousand per microliter)', 'Blood test result',
+
+# List of categorical columns to encode
+categorical_unordered_columns = [
+    'Genes in mother\'s side', 'Inherited from father', 'Maternal gene', 'Paternal gene',
+    'Gender', 'Birth asphyxia',
+    'Autopsy shows birth defect (if applicable)', 'Place of birth',
+    'Folic acid details (peri-conceptional)', 'H/O serious maternal illness',
+    'H/O radiation exposure (x-ray)', 'H/O substance abuse', 'Status',
+    'Assisted conception IVF/ART', 'History of anomalies in previous pregnancies',
     'Symptom 1', 'Symptom 2', 'Symptom 3', 'Symptom 4', 'Symptom 5'
 ]
+
+quantitative_with_unknowns_or_ordered_columns = ['Patient Age', "Mother's age", "Father's age", 'Respiratory Rate (breaths/min)',
+    'Heart Rate (rates/min)', 'Follow-up', 'No. of previous abortion',
+    'Birth defects', 'White Blood cell count (thousand per microliter)', 'Blood test result']
 
 # Train models without unknown targets
 
@@ -37,22 +40,22 @@ df.dropna(inplace=True)
 # Prepare catgorical data
 
 df_encoded=df.copy()
-for column in all_x_columns:
+for column in quantitative_with_unknowns_or_ordered_columns:
     df_encoded[column] = label_encoder.fit_transform(df_encoded[column].astype(str))
-
+df_encoded=pd.get_dummies(df_encoded, columns=categorical_unordered_columns, drop_first=False)
 
 # Train model for the second-to-last column
 
 # Extract features and target
-X = df_encoded.iloc[:, :-2]
-y = df_encoded.iloc[:, -2]
+X = df_encoded.drop(columns=['Genetic Disorder', 'Disorder Subclass'], axis=1)
+y = df_encoded['Genetic Disorder']
 
 # Split the data into validation, validation and test
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75,  random_state=1) # training as 75% 
 
 # Model used
-nb = GaussianNB()
+nb = MultinomialNB()
 nb.fit(X_train, y_train)
 
 y_pred = nb.predict(X_test) 
@@ -65,12 +68,12 @@ print(classification_report(y_test, y_pred))
 
 
 # Train model for the last column
-y = df_encoded.iloc[:, -1]
+y = df_encoded['Disorder Subclass']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75,  random_state=1) # training as 75% 
 
 # Model used
-nb = GaussianNB()
+nb = MultinomialNB()
 nb.fit(X_train, y_train)
 
 y_pred = nb.predict(X_test) 
@@ -92,22 +95,23 @@ df.dropna(inplace=True)
 # Prepare catgorical data
 
 df_encoded=df.copy()
-for column in all_x_columns:
+for column in quantitative_with_unknowns_or_ordered_columns:
     df_encoded[column] = label_encoder.fit_transform(df_encoded[column].astype(str))
+df_encoded=pd.get_dummies(df_encoded, columns=categorical_unordered_columns, drop_first=False)
 
 
 # Train model for the second-to-last column
 
 # Extract features and target
-X = df_encoded.iloc[:, :-2]
-y = df_encoded.iloc[:, -2]
+X = df_encoded.drop(columns=['Genetic Disorder', 'Disorder Subclass'], axis=1)
+y = df_encoded['Genetic Disorder']
 
 # Split the data into validation, validation and test
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75,  random_state=1) # training as 75% 
 
 # Model used
-nb = GaussianNB()
+nb = MultinomialNB()
 nb.fit(X_train, y_train)
 
 y_pred = nb.predict(X_test) 
@@ -120,12 +124,12 @@ print(classification_report(y_test, y_pred))
 
 
 # Train model for the last column
-y = df_encoded.iloc[:, -1]
+y = df_encoded['Disorder Subclass']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75,  random_state=1) # training as 75% 
 
 # Model used
-nb = GaussianNB()
+nb = MultinomialNB()
 nb.fit(X_train, y_train)
 
 y_pred = nb.predict(X_test) 
